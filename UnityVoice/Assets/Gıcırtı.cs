932b2a39a62fcf3419584fee051ad720a81bf166
Gaye Yılmaz <gaye3545@gmail.com>
2022-03-13 20:30:58 +0300

    Audio practice

---
+++ UnityVoice/Assets/Gıcırtı.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gıcırtı : MonoBehaviour
+{
+   // public Vector3 changePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localPosition = new Vector3(3, 3, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.localScale += changePos;
+    }
+}
